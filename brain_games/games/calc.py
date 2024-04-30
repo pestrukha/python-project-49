dc6acd04e8ca6133dd3adcf5dc52d8c0625e3cbc
pestrukha <sturbauen@gmail.com>
2024-04-30 15:05:54 +0400

    fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,13 +1,14 @@
 import random
+import operator
 
 INTRO = 'What is the result of the expression?'
 
 
 def get_question_and_correct_answer():
-    operators = ['+', '-', '*']
+    operators = [('+', operator.add), ('-', operator.sub), ('*', operator.mul)]
     num1 = random.randint(1, 20)
     num2 = random.randint(1, 20)
-    operation = random.choice(operators)
+    operation, function = random.choice(operators)
     question = f'{num1} {operation} {num2}'
-    correct_answer = str(eval(question))
+    correct_answer = str(function(num1, num2))
     return correct_answer, question
