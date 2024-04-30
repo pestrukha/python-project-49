bcd9ad6fccd5db3d9f11e04a26b669abd0e83063
pestrukha <sturbauen@gmail.com>
2024-04-30 14:50:57 +0400

    fix gcd and prime

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,15 +3,15 @@ import random
 INTRO = 'Find the greatest common divisor of given numbers.'
 
 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
 def get_question_and_correct_answer():
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     question = f'{num1} {num2}'
-
-    def gcd(a, b):
-        while b:
-            a, b = b, a % b
-        return a
-
     correct_answer = str(gcd(num1, num2))
     return correct_answer, question
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,15 +3,16 @@ import random
 INTRO = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def get_question_and_correct_answer():
-    def is_prime(num):
-        if num <= 1:
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
             return False
-        for i in range(2, num):
-            if num % i == 0:
-                return False
-        return True
+    return True
 
+
+def get_question_and_correct_answer():
     question = random.randint(1, 100)
     correct_answer = 'yes' if is_prime(question) is True else 'no'
     return correct_answer, question
